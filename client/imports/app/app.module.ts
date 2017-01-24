e2a4dfa7698c58e6e64c3d3f9cb24408220c0dc4
Valter Gomes <valter.junior@soaexpert.com.br>
2017-01-24 21:02:09 -0200

    socially - step 17

---
+++ client/imports/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AccountsModule } from 'angular2-meteor-accounts-ui';
+import { Ng2PaginationModule } from 'ng2-pagination';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
+import { AppComponent } from './app.component';
+import { routes, ROUTES_PROVIDERS } from './app.routes';
+import { PARTIES_DECLARATIONS } from './parties';
+import { SHARED_DECLARATIONS } from './shared';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    AccountsModule,
+    Ng2PaginationModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAWoBdZHCNh5R-hB5S5ZZ2oeoYyfdDgniA'
+    })
+  ],
+  declarations: [
+    AppComponent,
+    ...PARTIES_DECLARATIONS,
+    ...SHARED_DECLARATIONS
+  ],
+  providers: [
+    ...ROUTES_PROVIDERS
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {}
